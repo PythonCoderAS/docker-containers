4bffe8c4d5efe38f45db9e18d1403b1e43b97c7d
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2023-11-29 21:10:52 -0500

    Fix

--- .github/workflows/get-valid-package-names.sh
+++ .github/workflows/get-valid-package-names.sh
@@ -4,7 +4,7 @@ first=0
 root="$(dirname "$(dirname "$(dirname "$(realpath $0)")")")"/containers
 for dockerfile in $(find "$root" -name Dockerfile)
 do
-  dirname="$(basename "$(dirname $dockerfile)")"
+  dirname="$(dirname $dockerfile)"
   if [ $first -eq 0 ]
   then
     printf "\"$dirname\""
